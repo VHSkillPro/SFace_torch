f87892f05204aba1e414d4f1928b3aa940c8033f
Hazzu <hailaconsutu558@gmail.com>
2025-04-23 14:23:52 +0700

    Add downscaling script for CASIA WebFace dataset and update training configurations

--- train.sh
+++ train.sh
@@ -8,17 +8,18 @@ CUDA_VISIBLE_DEVICES='0' python3 -u train_SFace_torch.py \
     --target lfw \
     --outdir ./results/mobilefacenet-sface-casia \
     --param_a 0.87 \
-    --param_b 1.2 2>&1|tee ./logs/mobilefacenet-sfacce-casia.log
+    --param_b 1.2 2>&1|tee ./logs/mobilefacenet-sface-casia.log
 
 CUDA_VISIBLE_DEVICES='0' python3 -u train_SFace_torch_freeze_backbone.py \
     --workers_id 0 \
     --batch_size 256 \
+    --epochs 50 \
     --lr 0.1 \
-    --stages 50,70,80 \
-    --data_root datasets/train/faces_webface_112x112 \
+    --stages 20,30,40 \
+    --data_root datasets/train/downscale_casia-webface_2_converted \
     --eval_path datasets/eval \
-    --resume_backbone weights/face_recognition_sface_2021dec.pth
-    --target lfw \
-    --outdir ./results/mobilefacenet-sface-casia \
+    --resume_backbone weights/face_recognition_sface_2021dec.pth \
+    --target lfw,cplfw,cfp_fp,cfp_ff,calfw,agedb_30 \
+    --outdir ./results/mobilefacenet-frezze_sface_casia-downscale \
     --param_a 0.87 \
-    --param_b 1.2 2>&1 | tee ./logs/mobilefacenet-sfacce-casia.log
+    --param_b 1.2 2>&1 | tee ./logs/mobilefacenet-frezze_sface_casia-downscale.log
